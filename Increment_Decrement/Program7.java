2a9b7e9e60fe2d1f89989a65e1c69af4c3ff4ed9
prakhar17072004 <pmankur2004@gmail.com>
2024-10-14 15:27:25 +0530

    new program of increment and decrement is added

---
+++ Increment_Decrement/Program7.java
@@ -0,0 +1,15 @@
+class Program7 
+{
+  public static void main(String[] arg)
+  {
+    int i=19;int j=29; int k;
+    k= i-- - i++ + --j - ++j + --i -j-- + ++i - j++;
+    System.out.println(i);
+    System.out.println(j);
+    System.out.println(k);
+    
+  }
+ }
+  
+    
+    
