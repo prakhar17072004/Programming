60467814035f873a0b47e461265f3f0bc438942f
prakhar17072004 <pmankur2004@gmail.com>
2024-10-20 00:06:25 +0530

    new added program of switch case

--- Switch_Case/MaxNum.java
+++ Switch_Case/MaxNum.java
@@ -1,4 +1,4 @@
-import.java.util.Scanner;
+import java.util.Scanner;
 class MaxNum
 {
 
@@ -7,17 +7,30 @@ class MaxNum
  
    Scanner sc = new Scanner(System.in);
     System.out.println("Enter the Number1: ");
-    int a = nextInt();
+    int a = sc.nextInt();
     System.out.println("Enter the Number2: ");
-    int b = nextInt();
-    int res = a>b?0:1;
+    int b = sc.nextInt();
+    int res = a>b?1:0;
     switch(res){
          case 0 : 
-                 res= a==b?1:0
-                 switch(res
-         
-                 System.out.println(" Number 1 is greater than 1");
-                 break;
-         
-         case 1 : System.out.println(" Number 2 is greater than 1");
+                 res = a==b?1:0;
+                 switch(res){
+                      case 0:
+                             System.out.println(" Number 1 is greater than Number 2" +b);
+                              break;
+                      case 1: System.out.println("Number 1 and Number 2 is equal "+a + " and " + b );        
+                               break;
+                               
+                            } 
+                            break;  
+         case 1 : System.out.println(" Number 1 is greater than Number 2" +a);
                  break;        
+                 
+                 
+                
+                
+                }
+                
+             }
+           
+           }  
