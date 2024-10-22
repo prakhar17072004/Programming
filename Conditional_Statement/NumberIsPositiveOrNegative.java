c524a22c5402d73e60bfb4b3715118de270bf521
prakhar17072004 <pmankur2004@gmail.com>
2024-10-22 22:20:20 +0530

    new program is added

---
+++ Conditional_Statement/NumberIsPositiveOrNegative.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class NumberIsPositiveOrNegative
+{
+  public static void main(String[] arg)
+  {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the number");
+  int s = sc.nextInt();
+  
+  if(s>0)
+  {
+    System.out.println("Number is postive");
+    
+  }
+  else{
+    System.out.println("number is negative");
+    }  
+    
+    
+  }  
+  
+ } 
