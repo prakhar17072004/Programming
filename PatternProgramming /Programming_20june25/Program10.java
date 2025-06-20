d1612f2cd228b7532a68c1b8e3ce0aef49c5be1a
prakhar17072004 <pmankur2004@gmail.com>
2025-06-20 22:43:40 +0530

    star pattern is added

---
+++ PatternProgramming /Programming_20june25/Program10.java
@@ -0,0 +1,12 @@
+class Program10{
+ public static void main(String args[]){
+  for(int i=1;i<=5;i++){
+   for(int j=1;j<=5;j++){
+    
+      System.out.print("(" + j + "," + i+ ")");
+      }
+      System.out.println();
+     }
+   }
+  } 
+  
---
+++ PatternProgramming /Programming_20june25/Program13.java
@@ -0,0 +1,17 @@
+class Program13{
+ public static void main(String args[]){
+  for(int i=1;i<=5;i++){
+   for(int j=1;j<=5;j++){
+    if(i==1||i==2||j==1||j==2||i==4||i==5||j==4||j==5){
+      System.out.print("* ");
+      }
+      else{
+       System.out.print("  ");
+       }
+       
+      }
+     System.out.println();
+     }
+    } 
+  }
+  
