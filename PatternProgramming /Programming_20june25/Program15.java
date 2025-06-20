560574e2125abcb0fd0d8ffc50777d372cf1b2a0
prakhar17072004 <pmankur2004@gmail.com>
2025-06-20 23:05:31 +0530

    plus pattern is added

---
+++ PatternProgramming /Programming_20june25/Program15.java
@@ -0,0 +1,16 @@
+class Program15{
+ public static void main(String args[]){
+  int mid =3;
+  for(int i= 1;i<=5;i++){
+   for(int j=1;j<=5;j++){
+    if(i==j||i+j==6){
+     System.out.print("* ");
+     }
+     else{
+      System.out.print("  ");
+      }
+     }
+    System.out.println();
+   } 
+  }
+ } 
