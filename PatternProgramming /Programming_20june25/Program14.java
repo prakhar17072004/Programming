531d5a849f127b0a024b6d0bcff6c231cbeac5af
prakhar17072004 <pmankur2004@gmail.com>
2025-06-20 22:55:47 +0530

    star program is added

---
+++ PatternProgramming /Programming_20june25/Program14.java
@@ -0,0 +1,16 @@
+class Program14{
+ public static void main(String args[]){
+  int mid =3;
+  for(int i= 1;i<=5;i++){
+   for(int j=1;j<=5;j++){
+    if(i==mid ||j==mid){
+     System.out.print("* ");
+     }
+     else{
+      System.out.print("  ");
+      }
+     }
+    System.out.println();
+   } 
+  }
+ } 
