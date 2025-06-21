8ed38bce1685b3995649501a4ff533d14a5d180a
prakhar17072004 <pmankur2004@gmail.com>
2025-06-21 23:47:52 +0530

    triangle pattern

---
+++ PatternProgramming /Programming_20june25/Program18.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Program18{
+
+ public static void main(String args[]){
+   
+    Scanner sc= new Scanner(System.in);
+    int star=1;
+    System.out.println("Enter the Number");
+    int n = sc.nextInt();
+    for(int i=1;i<=n;i++){
+     for(int j=1;j<=star;j++){
+      System.out.print("* ");
+      }
+      star++;
+      System.out.println();
+      }
+     } 
+     }
